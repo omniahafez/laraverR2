68a214a6741cadfd7ace2b6b7597feca11c64876
omniahafez <mezo30782@gmail.com>
2024-07-02 15:05:13 +0300

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontPage;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+   // return view('test');
+//});
+
+Route::get('/',[Frontpage::class,'home'])->name('home');
